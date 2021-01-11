6c356dc3b23b0fc2f3041df47b545b498b8867e7
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2021-01-11 17:37:49 +0400

    RecipeCreate component minor update

--- src/components/recipeCreate.js
+++ src/components/recipeCreate.js
@@ -1,30 +1,13 @@
 import { React, useState } from 'react';
 
+import { TextField } from '@material-ui/core';
+
+
 const recipeCreate = () => {
     return (
-        <div>
-            <section className="section-center">
-        <form className="grocery-form"> 
-            <h3>Grocery Bud</h3>
-            <div className="form-control">
-                <input 
-                    type="text" 
-                    className="grocery" 
-                    placeholder="e.g.eggs"
-                    
-                />
-                <button type="submit" className="submit-btn">
-                </button>
-            </div>
+        <form noValidate autoComplete="off">
+            <TextField id="standard-basic" label="Standard" />
         </form>
-            <div className="grocery-container">
-                {/* <List/> */}
-                <button className="clear-btn">
-                    clear items
-                </button>
-            </div>
-    </section>
-        </div>
     )
 }
 
