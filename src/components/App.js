54ff3cd44725e746816eb7a3273acadf2f1db44c
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-11 14:59:41 +0100

    Initial minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'   
-import * as RecipeCreate from "./recipeCreate";
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'  
+import  recipeCreate from "./recipeCreate";
+import  recipeGet from "./recipeGet";
+import '../App.css';
 
 const App = () => {
     return (
         <div>
             <Router>            
                 <Switch>
-                    <Route exact path="/" />
-                    <Route path="/recipes/new" component={RecipesCreate}/>
+                    <Route exact path="/"  component={recipeGet}/>
+                    <Route path="/recipes/new" component={recipeCreate}/>
                 </Switch>
             </Router>
         </div>
---
+++ src/components/recipeGet.js
@@ -0,0 +1,11 @@
+import { React, useState } from 'react';
+
+const recipeGet = () => {
+    return (
+        <div>
+            Get recipes here.
+        </div>
+    )
+}
+
+export default recipeGet;
